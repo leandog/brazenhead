47ecfb1b1a33bc37dfa224f1a8bf2c57150e6b2c
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-08-21 20:54:27 -0400

    added shell for command-line tool

---
+++ lib/gametel_driver/cli.rb
@@ -0,0 +1,10 @@
+require 'thor'
+
+module GametelDriver
+  class CLI < Thor
+    desc "build", "Build an apk that can be installed into an emulator or device"
+    def build
+      
+    end
+  end
+end
