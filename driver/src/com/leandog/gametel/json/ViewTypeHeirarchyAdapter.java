837adef7bcba97724a1e1f98c24462d06155aa10
Levi Wilson <levi@leviwilson.com>
2012-08-25 00:01:43 -0400

    returns more useful information for a View

--- driver/src/com/leandog/gametel/json/ViewTypeHeirarchyAdapter.java
+++ driver/src/com/leandog/gametel/json/ViewTypeHeirarchyAdapter.java
@@ -7,10 +7,37 @@ import android.view.View;
 import com.google.gson.*;
 
 public class ViewTypeHeirarchyAdapter implements JsonSerializer<View> {
-    
+
+    @SuppressWarnings("unused")
     public class JsonView {
+        private final int id;
+        private final String classType;
+        
+        private final int width;
+        private final int height;
+        
+        private final int[] screenLocation = new int[2];
+        private final int[] windowLocation = new int[2];
+        
+        private final int left;
+        private final int top;
+        private final int right;
+        private final int bottom;
 
         public JsonView(final View theView) {
+            id = theView.getId();
+            classType = theView.getClass().getName();
+            
+            width = theView.getWidth();
+            height = theView.getHeight();
+            
+            theView.getLocationOnScreen(screenLocation);
+            theView.getLocationInWindow(windowLocation);
+            
+            left = theView.getLeft();
+            top = theView.getTop();
+            right = theView.getRight();
+            bottom = theView.getBottom();
         }
     }
 
