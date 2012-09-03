a3a60ae288024f239d7c5e076dabba57e081a3a3
Levi Wilson <levi@leviwilson.com>
2012-09-03 10:18:16 -0400

    process redirects stdout and stderr to a file

--- lib/gametel_driver/process.rb
+++ lib/gametel_driver/process.rb
@@ -2,7 +2,12 @@ module GametelDriver
   class Process
     def run(process, *args)
       process = ChildProcess.build(process, *args)
+      process.io.stdout, process.io.stderr = std_out_err
       process.wait
+    end
+
+    def std_out_err
+      return ::Tempfile.new("gametel-proc-out-#{Time.now}"), ::Tempfile.new("gametel-proc-err-#{Time.now}")
     end
 
   end
--- spec/lib/gametel_driver/process_spec.rb
+++ spec/lib/gametel_driver/process_spec.rb
@@ -1,7 +1,15 @@
+require 'spec_helper'
+
 describe Process do
   before(:each) do
     @running_process = double('process')
     @running_process.stub(:wait)
+
+    @io = double('io')
+    @io.stub(:stdout=)
+    @io.stub(:stderr=)
+    @running_process.stub(:io) { @io }
+
     ChildProcess.stub(:build) { @running_process }
     @process = GametelDriver::Process.new
   end
@@ -13,6 +21,17 @@ describe Process do
 
   it "should wait for the command to complete" do
     @running_process.should_receive(:wait)
+    @process.run('anything')
+  end
+
+  it "it redirects stdout and stderr" do
+    out = double('stdout')
+    err = double('stderr')
+    Time.should_receive(:now).and_return('this-time', 'next-time')
+    Tempfile.should_receive(:new).with('gametel-proc-out-this-time').and_return(out)
+    Tempfile.should_receive(:new).with('gametel-proc-err-next-time').and_return(err)
+    @io.should_receive(:stdout=).with(out)
+    @io.should_receive(:stderr=).with(err)
     @process.run('anything')
   end
 end
