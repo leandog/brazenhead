54edb8eb940d211e82895e3598bdd0fe628bc35f
Levi Wilson <levi@leviwilson.com>
2012-09-03 21:24:32 -0400

    preserves last stdout and stderr

--- lib/gametel_driver/process.rb
+++ lib/gametel_driver/process.rb
@@ -1,13 +1,24 @@
 module GametelDriver
   class Process
+
+    attr_accessor :last_stdout, :last_stderr
+
     def run(process, *args)
       process = ChildProcess.build(process, *args)
       process.io.stdout, process.io.stderr = std_out_err
       process.wait
+      @last_stdout = output(process.io.stdout)
+      @last_stderr = output(process.io.stderr)
     end
 
     def std_out_err
       return ::Tempfile.new("gametel-proc-out-#{Time.now}"), ::Tempfile.new("gametel-proc-err-#{Time.now}")
+    end
+
+    private
+    def output(file)
+      file.rewind
+      file.read
     end
   end
 end
