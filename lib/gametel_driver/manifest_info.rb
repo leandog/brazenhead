28c06ebe7d3bb305a442268b61f47aaf0a63c146
Levi Wilson <levi@leviwilson.com>
2012-09-03 22:28:10 -0400

    can grab min and target sdk from the manifest

--- lib/gametel_driver/manifest_info.rb
+++ lib/gametel_driver/manifest_info.rb
@@ -2,6 +2,19 @@ require 'gametel_driver/process'
 
 module GametelDriver
   class ManifestInfo
+    def min_sdk
+      match = /android:minSdkVersion.*=\(.*\)0x(\h+)/.match manifest
+      min = 1
+      min = match.captures[0].hex unless match.nil?
+      min
+    end
+
+    def target_sdk
+      match = /android:targetSdkVersion.*=\(.*\)0x(\h+)/.match manifest
+      target = match.captures[0].hex unless match.nil?
+      target
+    end
+
     def initialize(apk)
       @apk = apk
       manifest
@@ -9,7 +22,12 @@ module GametelDriver
 
     private
     def manifest
+      @manifest ||= dump_manifest
+    end
+
+    def dump_manifest
       process.run('aapt', 'dump', 'xmltree', @apk, 'AndroidManifest.xml')
+      process.last_stdout
     end
 
     def process
--- spec/lib/gametel_driver/manifest_info_spec.rb
+++ spec/lib/gametel_driver/manifest_info_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe GametelDriver::ManifestInfo do
-  let(:process) { double('gametel-process') }
+  let(:process) { double('gametel-process').as_null_object }
+  let(:manifest_info) { GametelDriver::ManifestInfo.new 'some_apk.apk' }
 
   before(:each) do
     GametelDriver::Process.stub(:new).and_return(process)
@@ -9,7 +10,42 @@ describe GametelDriver::ManifestInfo do
 
   it "should load the manifest upon initialize" do
     process.should_receive(:run).with('aapt', 'dump', 'xmltree', 'some_apk.apk', 'AndroidManifest.xml')
-    GametelDriver::ManifestInfo.new 'some_apk.apk'
+    manifest_info
+  end
+
+  it "should grab the minimum sdk" do
+    process.should_receive(:last_stdout).and_return("
+E: uses-sdk (line=39)
+A: android:minSdkVersion(0x0101020c)=(type 0x10)0x0f
+A: android:targetSdkVersion(0x01010270)=(type 0x10)0xe")
+
+    manifest_info.min_sdk.should eq 15
+  end
+
+  it "should default the minimum sdk to 1" do
+    process.should_receive(:last_stdout).and_return("
+E: uses-sdk (line=39)
+A: android:notTheminSdkVersion(0x0101020c)=(type 0x10)0x0f
+A: android:targetSdkVersion(0x01010270)=(type 0x10)0xe")
+
+    manifest_info.min_sdk.should eq 1
+  end
+
+  it "should grab the target sdk" do
+    process.should_receive(:last_stdout).and_return("
+E: uses-sdk (line=39)
+A: android:notTheminSdkVersion(0x0101020c)=(type 0x10)0x0f
+A: android:targetSdkVersion(0x01010270)=(type 0x10)0xe")
+
+    manifest_info.target_sdk.should eq 14
+  end
+
+  it "should default the target sdk" do
+    process.should_receive(:last_stdout).and_return("
+E: uses-sdk (line=39)
+A: android:notTheminSdkVersion(0x0101020c)=(type 0x10)0x0f")
+
+    manifest_info.target_sdk.should be_nil
   end
 
 end
