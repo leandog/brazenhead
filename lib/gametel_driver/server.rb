c26514f14eb26e43f4e771bf985d0433afec82b3
Levi Wilson <levi@leviwilson.com>
2012-09-05 01:44:11 -0400

    refactor server

--- lib/gametel_driver/server.rb
+++ lib/gametel_driver/server.rb
@@ -4,23 +4,45 @@ module GametelDriver
       validate(apk)
 
       Dir.mktmpdir do |dir|
-        copy_base_files_to dir
-        manifest_info = GametelDriver::ManifestInfo.new(apk)
-        the_target = manifest_info.package
-        contents = File.read(File.join(dir, manifest))
-        File.write(File.join(dir, manifest), contents.gsub(/\btargetPackage="[^"]+"/, "targetPackage=\"#{the_target}\""))
+        copy_base_files_to(dir)
+        update_target_in(dir)
       end
     end
 
     private
     def validate(apk)
+      @source_apk = apk
       invalid_package_err unless package_exists(apk)
     end
 
+    def update_target_in(dir)
+      File.write(manifest_path(dir), manifest_contents(dir).gsub(target_match, target_replace))
+    end
+
+    def target_match
+      /\btargetPackage="[^"]+"/
+    end
+
+    def target_replace
+      "targetPackage=\"#{the_target}\""
+    end
+
+    def manifest_contents(dir)
+      File.read(manifest_path(dir))
+    end
+
+    def the_target
+      @the_target ||= GametelDriver::ManifestInfo.new(@source_apk).package
+    end
+
+    def manifest_path(dir)
+      File.join(dir, manifest)
+    end
+
     def copy_base_files_to(dir)
-        base_files.each do |file|
-          File.copy_file("../../#{file}", dir)
-        end
+      base_files.each do |file|
+        File.copy_file("../../#{file}", dir)
+      end
     end
 
     def base_files
