4bdef96c85117c0c6dd22b536f957eda7131c1bd
Levi Wilson <levi@leviwilson.com>
2012-09-04 23:19:57 -0400

    refactor server

--- lib/gametel_driver/server.rb
+++ lib/gametel_driver/server.rb
@@ -4,9 +4,7 @@ module GametelDriver
       validate(info)
 
       Dir.mktmpdir do |dir|
-        ['gametel_driver-release-unsigned.apk', 'AndroidManifest.xml'].each do |file|
-          File.copy_file("../../#{file}", dir)
-        end
+        copy_base_files_to dir
       end
     end
 
@@ -14,6 +12,16 @@ module GametelDriver
     def validate(info)
       missing_argument_err unless info[:package]
       invalid_package_err unless package_exists(info[:package])
+    end
+
+    def copy_base_files_to(dir)
+        base_files.each do |file|
+          File.copy_file("../../#{file}", dir)
+        end
+    end
+
+    def base_files
+      ['gametel_driver-release-unsigned.apk', 'AndroidManifest.xml']
     end
 
     def package_exists(package)
