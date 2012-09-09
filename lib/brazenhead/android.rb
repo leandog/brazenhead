7d0db32d6b6dda744e17ddac217ad132897bac82
Levi Wilson <levi@leviwilson.com>
2012-09-09 11:26:57 -0400

    raises error if android.jar path does not exist

--- lib/brazenhead/android.rb
+++ lib/brazenhead/android.rb
@@ -1,11 +1,30 @@
 module Brazenhead
   module Android
     def path_to(sdk)
-      File.join(ENV['ANDROID_HOME'], "platforms/android-#{sdk}", 'android.jar')
+      jar = android_jar(sdk)
+      android_jar_missing(sdk) unless File.exists? jar
+      jar
     end
 
     def default_key_path
       File.expand_path("~/.android/debug.keystore")
+    end
+
+    private
+    def android_home
+      ENV['ANDROID_HOME']
+    end
+
+    def platform(sdk)
+      "platforms/android-#{sdk}"
+    end
+
+    def android_jar(sdk)
+      File.join(android_home, platform(sdk), 'android.jar')
+    end
+
+    def android_jar_missing(sdk)
+      raise Exception, "the path to android-#{sdk} was not found"
     end
   end
 end
--- spec/lib/brazenhead/android_spec.rb
+++ spec/lib/brazenhead/android_spec.rb
@@ -6,12 +6,27 @@ end
 
 describe Brazenhead::Android do
   let(:android) { AndroidTest.new }
+
   context "resolving android paths" do
+
+    before(:each) do
+      ENV.stub(:[]).with('ANDROID_HOME').and_return('/path/to/android')
+      File.stub(:exists?).and_return(true)
+    end
+
     it "should be able to locate the android jar" do
       ENV.stub(:[]).with('ANDROID_HOME').and_return('/path/to/android')
       android.path_to(8).should eq '/path/to/android/platforms/android-8/android.jar'
     end
 
+    it "should raise an error if the path is not found" do
+      File.should_receive(:exists?).and_return(false)
+      api = 8
+      lambda { android.path_to(api) }.should raise_error (message="the path to android-#{api} was not found")
+    end
+  end
+
+  context "locating the keystore" do
     it "should be able to locate the path to the default keystore" do
       File.stub(:expand_path).with("~/.android/debug.keystore").and_return("/some/expanded/.android/debug.keystore")
       android.default_key_path.should eq "/some/expanded/.android/debug.keystore"
